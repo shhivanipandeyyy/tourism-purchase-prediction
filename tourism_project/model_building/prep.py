3a0578bbe2fd476841a247452b78ca16fdaf1cb6
Shivani Pandey <shhivanipandeyyy@gmail.com>
2025-11-24 14:02:55 +0530

    Add And Update prep.py

--- tourism_project/model_building/prep.py
+++ tourism_project/model_building/prep.py
@@ -1,1 +1,62 @@
+%%writefile tourism_project/model_building/prep.py
+
+# for data manipulation
+import pandas as pd
+import sklearn
+# for creating a folder
+import os
+# for data preprocessing and pipeline creation
+from sklearn.model_selection import train_test_split
+# for converting text data in to numerical representation
+from sklearn.preprocessing import LabelEncoder
+# for hugging face space authentication to upload files
+from huggingface_hub import login, HfApi
+
+import os
+from huggingface_hub import HfApi
+
+
+DATASET_PATH =  "tourism_project/data/tourism.csv"
+
+df = pd.read_csv(DATASET_PATH)
+print("Dataset loaded successfully.")
+
+
+df.drop(columns=['CustomerID'], inplace=True)
+
+from sklearn.preprocessing import LabelEncoder
+
+label_encoder = LabelEncoder()
+df['TypeofContact'] = label_encoder.fit_transform(df['TypeofContact'])
+
+
+target_col = 'ProdTaken'
+y = df[target_col]
+X = df.drop(columns=[target_col])
+
+# Split into X (features) and y (target)
+X = df.drop(columns=[target_col])
+y = df[target_col]
+
+# Perform train-test split
+Xtrain, Xtest, ytrain, ytest = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+Xtrain.to_csv("Xtrain.csv",index=False)
+Xtest.to_csv("Xtest.csv",index=False)
+ytrain.to_csv("ytrain.csv",index=False)
+ytest.to_csv("ytest.csv",index=False)
+
+
+files = ["Xtrain.csv","Xtest.csv","ytrain.csv","ytest.csv"]
+
+for file_path in files:
+    api.upload_file(
+        path_or_fileobj=file_path,
+        path_in_repo=file_path.split("/")[-1],  # just the filename
+        repo_id="shivaniPandey/tourism-purchase-predictor",
+        repo_type="dataset",
+    )
+
 
